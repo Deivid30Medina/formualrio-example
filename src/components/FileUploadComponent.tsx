38222e8f7845af41ba3a117cf2eb5a6ab6d80327
Deivid30Medina <deivis32sebas02@gmail.com>
2024-10-29 13:37:53 -0500

    styles

--- src/components/FileUploadComponent.tsx
+++ src/components/FileUploadComponent.tsx
@@ -17,7 +17,7 @@ const FileUploadComponent = () => {
         onChange={handleFileChange}
         accept={allowedFileTypesOptions.map((ext) => `.${ext}`).join(", ")}
       />
-      <p lassName="text-lg px-5 text-red-600 font-bold animate-scale-infinite">
+      <p className="text-lg px-5 text-red-600 font-bold animate-scale-infinite">
         Máximo 1 fichero.
         <br />
         límite de 100 MB.
